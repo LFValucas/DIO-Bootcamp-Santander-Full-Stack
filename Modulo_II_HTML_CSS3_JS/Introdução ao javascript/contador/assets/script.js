c6e69769fa32a598911ea4abf574802fc2977ef9
LFValucas <106846831+LFValucas@users.noreply.github.com>
2022-06-14 19:35:13 -0300

    Criado desafio contador
    Atividade do curso Introdução ao javascript

---
+++ Modulo_II_HTML_CSS3_JS/Introdução ao javascript/contador/assets/script.js
@@ -0,0 +1,28 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment(){
+    if(currentNumber < 10){
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber; 
+    }
+    if(currentNumber > 0){
+        currentNumberWrapper.style.color = 'blue';
+    }
+    if(currentNumber == 0){
+        currentNumberWrapper.style.color = 'black';
+    }
+}
+
+function decrement(){
+    if(currentNumber > -10){
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    } 
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
+    if(currentNumber == 0){
+        currentNumberWrapper.style.color = 'black';
+    }
+}
