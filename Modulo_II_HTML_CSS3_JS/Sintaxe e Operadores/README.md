af8f4f40bd9506aa91ed292e22289b58831e89a4
LFValucas <106846831+LFValucas@users.noreply.github.com>
2022-06-16 22:02:53 -0300

    Atividade compara números finalizada

---
+++ Modulo_II_HTML_CSS3_JS/Sintaxe e Operadores/README.md
@@ -0,0 +1,9 @@
+# Compara Números
+
+Esse programa tem como objetivo comparar dois números e ver se eles são iguais ou diferentes e se sua soma é maior ou menor que 20.
+
+* A entrada de valores só aceita números
+* Programa impede o avanço sem todos dados serem preenchidos
+* O Front End é responsivo
+
+![Front End da aplicação Compara Números](./assets/comparanumeros.jpg)
--- Modulo_II_HTML_CSS3_JS/Sintaxe e Operadores/assets/script.js
+++ Modulo_II_HTML_CSS3_JS/Sintaxe e Operadores/assets/script.js
@@ -1,8 +1,29 @@
-function comparaNumeros(num1, num2){
-    const iguais = num1 === num2;
-    const soma = num1 + num2;
-
-    if(iguais){
-        return "São Iguais"
+function comparaNumeros(){
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var resultado1 = document.getElementById('resultado');
+    const soma = parseInt(num1) + parseInt(num2);
+    if(num1 == '' && num2 == ''){
+        resultado1.innerHTML = 'Nenhum número inserido';
     }
+    else if(num1 == '' || num2 == ''){
+        resultado1.innerHTML = 'Falta inserir mais um número';
+    }
+    if(num1 != '' && num2 != ''){
+        if(num1 == num2){
+            var texto1 = 'Os Valores são iguais';
+        }
+        if(num1 != num2){
+            var texto1 = 'Os valores são diferentes';
+        }
+        if(soma > 20){
+            var texto2 = 'A soma de '+num1+' com '+num2+' é maior que 20';
+        }
+        if(soma < 20){
+            var texto2 = 'A soma de '+num1+' com '+num2+' é menor que 20';
+        }
+
+        resultado1.innerHTML = texto1+'</br>'+texto2;
+    }
+    
 }
