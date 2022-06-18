fca17fcacf02dd060b4fa832d4aa0350e8575e56
LFValucas <106846831+LFValucas@users.noreply.github.com>
2022-06-17 23:26:07 -0300

    Atividade prática verifica palíndromo finalizada

---
+++ Modulo_II_HTML_CSS3_JS/Variaveis-e-tipos/README.md
@@ -0,0 +1,3 @@
+# Módulo II - Variáveis e Tipos - Atividade Prática - Verificar palíndromo
+
+Estou compartilhando a solução que desenvolvi para o código conseguir verificar os mais diversos tipos de palíndromos, com espaços, letras maiúsculas e minusculas.
--- Modulo_II_HTML_CSS3_JS/Variaveis-e-tipos/script.js
+++ Modulo_II_HTML_CSS3_JS/Variaveis-e-tipos/script.js
@@ -1,1 +1,14 @@
-console.log('TESTE OK')
+function verificaPalindromo(string){
+    if(!string) return "string inexistente";
+
+    let texto1 = string.trim().split(" ").join("").toUpperCase();
+
+    let texto2 = texto1.split("").reverse().join("").toUpperCase();
+
+    return texto1 === texto2;
+}
+
+let myVar = "a Me o P    oe MA  "
+
+console.log("É um palíndromo? "+verificaPalindromo(myVar));
+
